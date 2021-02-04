cd72d5383c197b9617798aec34798411faafcfd0
annopo <pecorium@gmail.com>
2021-02-04 18:27:09 +0900

    study module

---
+++ menu.py
@@ -0,0 +1,19 @@
+class MenuItem:
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def info(self):
+        return self.name + ': ¥' + str(self.price)
+
+    def get_total_price(self, count):
+        total_price = self.price * count
+        return total_price
+
+menu_item1 = MenuItem('サンドイッチ', 500)
+
+print(menu_item1.info())
+
+result = menu_item1.get_total_price(4)
+print('合計は' + str(result) + '円です')
