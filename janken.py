0d528eb4266ae099fb056015b7fc819525317bf0
annopo <pecorium@gmail.com>
2021-02-04 18:04:38 +0900

    study library

--- janken.py
+++ janken.py
@@ -1,4 +1,5 @@
 import utils
+import random
 
 print('じゃんけんをはじめます')
 player_name = input('名前を入力してください：')
@@ -6,15 +7,16 @@ print('何を出しますか？（0: グー, 1: チョキ, 2: パー）')
 player_hand = int(input('数字で入力してください：'))
 
 if utils.validate(player_hand):
-    computer_hand = 1
-
+    # randintを用いて0から2までの数値を取得し、変数computer_handに代入
+    computer_hand = random.randint(0, 2)
+    
     if player_name == '':
         utils.print_hand(player_hand)
     else:
         utils.print_hand(player_hand, player_name)
 
-    utils.print_hand(computer_hand, 'コンピュータ')
-
+    utils.print_hand(computer_hand, 'コンピューター')
+    
     result = utils.judge(player_hand, computer_hand)
     print('結果は' + result + 'でした')
 else:
