f8698380b160447cf6f06736a005eea791178f41
annopo <pecorium@gmail.com>
2021-02-04 18:42:03 +0900

    create item list

--- menu.py
+++ menu.py
@@ -4,3 +4,12 @@ menu_item1 = MenuItem('サンドイッチ', 500)
 menu_item2 = MenuItem('チョコケーキ', 400)
 menu_item3 = MenuItem('コーヒー', 300)
 menu_item4 = MenuItem('オレンジジュース', 200)
+
+menu_items = [menu_item1, menu_item2, menu_item3, menu_item4]
+
+index = 0
+
+for item in menu_items:
+    print(str(index) + '. ' + item.info())
+    
+    index += 1
